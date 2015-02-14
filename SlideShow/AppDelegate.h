f983230731b33d559ad9fd4968b53ba1d16a2479
baljeetSingh21 <baljeet.singh@venturepact.com>
2015-02-14 10:21:49 +0530

    Initial Commit

---
+++ SlideShow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlideShow
+//
+//  Created by Vicky Mathneja on 14/02/15.
+//  Copyright (c) 2015 MINE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SlideShow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlideShow
+//
+//  Created by Vicky Mathneja on 14/02/15.
+//  Copyright (c) 2015 MINE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
